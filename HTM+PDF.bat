5c41f0c0e51eeea4fd0dbf0df1b2a521b9f3bcdc
Vasiliy Kostin <38539456+bacek97@users.noreply.github.com>
2022-11-02 13:24:16 +0300

    Update HTM+PDF.bat

--- HTM+PDF.bat
+++ HTM+PDF.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 chcp 65001
 
 rem Инструкция: 
@@ -17,20 +17,22 @@ rem Перетащите созданный файл HTM на файл HTM+PDF.b
 
 if not "%~x1" == ".htm" if not "%~x2" == ".htm" (echo --- Затащите на этот файл мышкой, файлы HTM и PDF --- && pause && exit)
 set "dp0=%~dp0"
-FOR /F "tokens=*" %%a IN ('type %dp0:~0,-1%:user.drive.id') DO set "gfid=%%a"
+@REM FOR /F "tokens=*" %%a IN ('type %dp0:~0,-1%:user.drive.id') DO set "gfid=%%a"
+@REM pause
+set "gfid=1Ty2o3zAJAvxZM6vZCi7JkSAGbizCZ9Q7"
 IF "%~x1" == ".pdf" (start /wait "" "C:\Program Files\SumatraPDF\SumatraPDF.exe" -print-to "HP_M132nw" -print-settings "3x" %~nx1)
 IF "%~x2" == ".pdf" (start /wait "" "C:\Program Files\SumatraPDF\SumatraPDF.exe" -print-to "HP_M132nw" -print-settings "3x" %~nx2) 
 echo --- Выполняется запрос doGet ---
-FOR /F "tokens=*" %%a IN ('..\Шаблоны\script\curl.exe -G --data-urlencode "fileName1=%~nx1" --data-urlencode "fileName2=%~nx2" --data-urlencode "folderId=%gfid%" -L "https://script.google.com/macros/s/AKfycbzHCHywgFHS1pMYQYh_HBD0H1tqHKpT54HKP-8OxGoaPnJZ3hA/exec"') DO set "response=%%a"
+FOR /F "tokens=*" %%a IN ('..\Шаблоны\script\curl.exe -G --data-urlencode "fileName1=%~nx1" --data-urlencode "fileName2=%~nx2" --data-urlencode "folderId=%gfid%" -L "https://script.google.com/macros/s/AKfycby4BQcVGGZeCRyMuHFAbaZKTOX-fVMraCYt7Me7XvthYpWR9YYI8T2RSSj1OKn8LVQR/exec"') DO set "response=%%a"
 for /F "tokens=1,2 delims=;" %%a in ("%response%") do (
-   start /wait "Скачивание сформированного договора" ..\Шаблоны\script\curl.exe -L "https://docs.google.com/document/d/%%b/export?exportFormat=pdf" -o Договор.pdf
-   echo --- Распечатка первой страницы договора --- 
+   timeout 15 && start /wait "Скачивание сформированного договора" ..\Шаблоны\script\curl.exe -L "https://docs.google.com/document/d/%%b/export?exportFormat=pdf" -o Договор.pdf && echo --- Распечатка первой страницы договора --- 
    start /wait "" "C:\Program Files\SumatraPDF\SumatraPDF.exe" -print-to "HP_M132nw" -print-settings "1,3x" Договор.pdf
    rem start /wait "" "C:\Program Files\SumatraPDF\SumatraPDF.exe" -page 1 -print-dialog -exit-when-done Договор.pdf
    echo --- Переверните распечатанную страницу договора --- 
    pause
    start /wait "" "C:\Program Files\SumatraPDF\SumatraPDF.exe" -print-to "HP_M132nw" -print-settings "2,3x" Договор.pdf
    echo --- После закрытия этого окна запустите файл СКАНИРОВАТЬ.bat ---
+   echo -!- ЗВОНИ 203: ОКНА, ПОДПИСЬ В ДОГОВОР -!----------------------------------------------
    timeout 20
    rem start /wait "" "C:\Program Files\SumatraPDF\SumatraPDF.exe" -page 2 -print-dialog -exit-when-done Договор.pdf
    move Договор.pdf "%%a"
